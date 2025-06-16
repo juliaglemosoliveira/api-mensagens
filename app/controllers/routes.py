14582d0465969eb7146ee2f1c9d66e26bef4ff70
juliaglemosoliveira <140888869+juliaglemosoliveira@users.noreply.github.com>
2025-06-16 13:16:31 +0000

    16/06

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -8,7 +8,15 @@ msg_bp = Blueprint('mensagens', __name__)
 @msg_bp.route('/mensagens', methods=['GET'])
 def read_all():
     mensagens = Entrada.query.all()
-    return jsonify([mensagem.json() for mensagem in mensagens])
+    return jsonify([mensagem.json() for mensagem in mensagens]), 200
+
+#Endpoint para READ ONE
+@msg_bp.route('/mensagens/<int:id>', methods=['GET'])
+def read_one(id):
+    mensagem = Entrada.query.get(id)
+    if mensagem:
+        return jsonify(mensagem.json()), 200
+    return {"Mensagem": "ID não encontrado, tente outro!"}, 404
 
 #Endpoint para CREATE
 @msg_bp.route('/mensagens', methods=['POST'])
@@ -18,5 +26,11 @@ def create_mensagem():
     db.session.add(nova_mensagem)
     db.session.commit()
     return jsonify(nova_mensagem.json()), 201
-    
 
+#Endpoint para DELETE
+@msg_bp.route('/mensagens/<int:id>', methods=['DELETE'])
+def delete_msg(id):
+    mensagem = Entrada.query.get(id)
+    db.session.delete(mensagem)
+    db.session.commit()
+    return {"Mensagem": "Mensagem excluída com sucesso!"}
